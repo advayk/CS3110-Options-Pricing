62127c02ade42319ed35acfa9c869f24dc327678
advaykoranne <advay.koranne@gmail.com>
2022-02-26 22:39:55 -0500

    added european option type for testing

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,18 @@
 let () = print_endline "CS 3110 Final Project: Options Pricing!"
+
+
+type european_option = { 
+stock_price : int ;  
+strike_price : int ; 
+time_to_expiration : int ; 
+risk_free_rate : float;  
+implied_volatility : float;
+}
+
+let create_european_option s k t r v = { 
+  stock_price = s ; 
+  strike_price = k ; time_to_expiration = t; 
+  risk_free_rate = r; implied_volatility = v
+}
+
+
