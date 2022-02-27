074fdd7ae424506eccc9e58f809858407ff5b8f3
advaykoranne <advay.koranne@gmail.com>
2022-02-27 16:51:20 -0500

    finished writing the days in between two dates -- including test funcions

--- _build/default/bin/blackscholes.ml
+++ _build/default/bin/blackscholes.ml
@@ -19,9 +19,21 @@ type date = {
   time : time 
 }
 
+(* [create_time h m s ms] creates a time type. 
+  Requires: 
+  hours (h) : between [0,24]
+  minute (m): between [0,60] 
+  seconds (s) : between [0,60]
+  milliseconds (s) : between [0,1000]*)
 let create_time h m s ms = {
   hour = h; minute = m; seconds = s; milliseconds = ms
 }
+
+(* create_date m d y t 
+  Requires:
+  month (m) : to be a valid number between [0,12]
+  day (d) : requires d to be a valid date for the month 
+  time (t) : required time to be a valid 24 hour time *)
 let create_date m d y (t: time)  = {month = m; day = d; year = y; time = t}
 
 (* [create_european_option s k t r v ] creates am european_option. 
@@ -38,12 +50,27 @@ let create_european_option k t r v = {
   implied_volatility = v
 }
 
+
+(* returns: true if [date1] comes strictly before [date2];
+ *   false otherwise
+ * requires: [date1] and [date2] are valid date triples
+*)
+let is_before date1 date2 =
+  date1.year < date2.year || (date1.year = date2.year && date1.month < date2.month) || 
+  (date1.year = date2.year && date1.month = date2.month && date1.month < date2.month)
+
+
+  
+(*[days_of_month m] returns the number of days in a given month. Requires m in between [0,12] *)
+let days_of_month m = 
+  if m = 1 ||  m = 3 || m = 5 ||  m = 7 ||  m = 8 ||  m = 10 ||  m = 12 then 31 else if m = 2 then 28 else 30
+
 (* [time_to_maturity date1 date2] takes two valid dates and computes the time in between in as a ratio of 1 year 
 example: one month would be 1/12 ~  0.0833...*)
-let time_to_maturity date1 date2 = 
-  raise (Failure "Unimplemented: time_to_maturity")
-
-
+let diff_between_dates date1 date2 =
+  let days_inbetwen = if is_before date1 date2 then ((days_of_month date1.month) - date1.day) + date2.day else (days_of_month date2.month) - date2.day + date1.day in 
+  print_endline (string_of_int days_inbetwen);
+  (float_of_int(days_inbetwen) /. 365.) 
 
 (* [strd_norm_cumulative_dist] is the standard normal cumulative distribution function. *)
 let strd_norm_cumulative_dist x = raise (Failure "Unimplemented: european_call")
--- _build/default/test/main.ml
+++ _build/default/test/main.ml
@@ -3,35 +3,34 @@ open Blackscholes
 
 let () = print_endline "CS 3110 Final Project: Options Pricing!"
 
-
+let close_enough a b =
+  Float.abs (a -. b) < 1e-2
+  
+  
 let european_call_options_price_test (name : string) (european_option : Blackscholes.european_option) 
 (current_stock_price : int) (current_time : int) 
 (expected_output : float) :
     test =
   name >:: fun _ ->
   (* the [printer] tells OUnit how to convert the output to a string *)
-  assert_equal expected_output (european_call_options_price european_option current_stock_price current_time) 
+  assert_equal expected_output (european_call_options_price european_option current_stock_price current_time) ~printer:string_of_float
 
-
-let time_to_maturity_test (name : string) (date1 : Blackscholes.date) (date2 : Blackscholes.date)  (expected_output : float) :
+let diff_between_dates_test (name : string) (date1 : Blackscholes.date) (date2 : Blackscholes.date) (expected_output : float) :
     test =
-  name >:: fun _ ->
-  assert_equal expected_output (time_to_maturity date1 date2) 
+  name >:: fun _ -> assert (close_enough expected_output (diff_between_dates date1 date2))
 
 let euro_option_1 = create_european_option 45 80 0.02 0.3 
 
-let time1 = create_time 22 0 0 0
-let time2 = create_time 11 0 0 0
+let time1 = create_time 0 0 0 0
+let time2 = create_time 0 0 0 0
 
-let date1 = create_date 1 1 2022 time1 
-let date2 = create_date 2 1 2022 time2
+let date1 = create_date 1 20 2022 time1 
+let date2 = create_date 2 6 2022 time2
 
 let blackscholes_test = [
   european_call_options_price_test "estimated call option price of euro_option_1" euro_option_1 6 0 6.02;
-  time_to_maturity_test "differnce betwen date1 and date2" date1 date2 0.0837
+  diff_between_dates_test "difference betwen date1 and date2" date1 date2 0.04657
 ]
-
-
 
 let tests =
   "test suite for blackscholes" >::: List.flatten
