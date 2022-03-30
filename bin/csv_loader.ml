ed5a647e5cbbe883c7b71f9a4f00669f3490dae9
Advay Koranne <ak845@cornell.edu>
2022-03-30 14:27:31 -0400

    update make stuff

--- bin/csv_loader.ml
+++ bin/csv_loader.ml
@@ -2,6 +2,10 @@ open ANSITerminal
 open Blackscholes
 open Csvreader
 
+let blackscholes_date  lst = 
+  let time = Blackscholes.create_time 0 0 0 0  in 
+  Blackscholes.create_date (int_of_string (List.nth lst 0)) ( int_of_string (List.nth lst 1)) (int_of_string (List.nth lst 2)) time
+
 
 let data_dir_prefix = "data" ^ Filename.dir_sep
 
@@ -13,7 +17,12 @@ let main () =
   print_endline "> ";
   match read_line () with
   | exception End_of_file -> ()
-  | file_name -> 
-    print_endline (Csvreader.(data_dir_prefix ^ file_name ^ ".csv") |> load_csv |> from_csv |> first)
+  | file_name -> print_endline
+  "Please enter the expiration date as (mm/dd/2022).";
+  match read_line () with
+    | exception End_of_file -> ()
+    | date -> let date_lst =  String.split_on_char '/' date in 
+    let bd = blackscholes_date date_lst in 
+    print_endline (Csvreader.(data_dir_prefix ^ file_name ^ ".csv") |> load_csv |> from_csv |> (Csvreader.delta bd))
     
 let () = main ()
